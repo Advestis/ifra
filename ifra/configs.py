18d691a81d849d95cfc2266e91fba1604e1230ce
pcotte <pcotte@advestis.com>
2022-01-13 17:08:50 +0100

    fix: try doc references

--- ifra/configs.py
+++ ifra/configs.py
@@ -32,7 +32,7 @@ class Config:
 
     Methods
     -------
-     `save()`-> None\n
+    save()-> None
         Saves the current configuration into the file it used to load. This allows the user to change the
         configuration in code and save it. Note that one can not have added a key not present in
         `Config.EXPECTED_CONFIGS`
