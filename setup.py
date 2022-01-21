436b3f3891e2f79837b073d7f2367abc9de368a7
pcotte <pcotte@advestis.com>
2022-01-21 13:01:56 +0100

    new: ignore tests in packages setup

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
         long_description=long_description,
         long_description_content_type="text/markdown",
         url=url,
-        packages=find_packages(),
+        packages=find_packages(exclude=("tests*")),
         install_requires=requirements,
         package_data={"": ["*", ".*"]},
         classifiers=[
